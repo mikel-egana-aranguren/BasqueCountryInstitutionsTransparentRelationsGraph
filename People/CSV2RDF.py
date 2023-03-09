9f1ea4dfa7d28630f178812d16067a0ca4e80d89
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2023-03-09 15:56:02 +0100

    Add roles

--- People/CSV2RDF.py
+++ People/CSV2RDF.py
@@ -18,20 +18,31 @@ output_graph = ConjunctiveGraph(identifier="https://data.ehu.eus/bcitr/people/gr
 
 # TODO: https://github.com/mikel-egana-aranguren/BasqueCountryInstitutionsTransparentRelationsGraph/issues/1
 base_uri = 'https://data.ehu.eus/bcitr/'
-person_uri = base_uri + "person/"
-schema_person_uri = "https://schema.org/Person"
+person_uri = base_uri + 'person/'
+schema_person_uri = 'https://schema.org/Person'
+role_uri = 'https://data.ehu.eus/bcitr/role/' # https://data.ehu.eus/bcitr/role/[normalised-person-name][officer|president|director|...][dates]
+company_uri = base_uri + "company/"
 
+has_role_prop = 'http://ehu.eus/tro#has_role'
 person_name_prop = "http://www.w3.org/ns/person#name"
 person_profile_prop = "http://ehu.eus/tro#profile_url"
-
-
 
 for row in input_file:
     # for k, v in row.items():
     #     print(k,v)
-
     name = row['name']
     profile = row['profile']
+    entity = row['entity']
+
+    # Codigo repetido de Registro Licitadores: mala idea
+    for row in input_file_licitadores:
+        entity_id = row['Número inscripción']
+        entity_name = row['Denominación social']
+        if entity_name == entity:
+            print(entity_id)
+            entity_uri = company_uri + entity_id
+
+
     # TODO: https://github.com/mikel-egana-aranguren/BasqueCountryInstitutionsTransparentRelationsGraph/issues/9
     normalised_name = name.replace(" ", "-").lower()
     person_uri_name = person_uri + normalised_name
@@ -39,6 +50,7 @@ for row in input_file:
     output_graph.add((URIRef(person_uri_name), RDF.type, URIRef(schema_person_uri)))
     output_graph.add((URIRef(person_uri_name), URIRef(person_name_prop), Literal(name)))
     output_graph.add((URIRef(person_uri_name), URIRef(person_profile_prop), Literal(profile)))
+    output_graph.add((URIRef(person_uri_name), URIRef(), URIRef()))
 
 output_graph.serialize(destination='People.nq', format='nquads', encoding="utf-8")
 
