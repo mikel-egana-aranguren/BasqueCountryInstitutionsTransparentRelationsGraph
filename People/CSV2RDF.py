15ff5aa55f8f9bc94f3d202b6f279a1e97748c5f
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2023-03-10 20:33:35 +0100

    Person example done

--- People/CSV2RDF.py
+++ People/CSV2RDF.py
@@ -10,6 +10,9 @@ import csv
 from rdflib import ConjunctiveGraph, Literal, Namespace, URIRef
 from rdflib.namespace import OWL, RDF, RDFS, SKOS, XSD, DC, DCTERMS, VOID
 
+def normalise_name(name):
+   return name.replace(" ", "-").lower().replace(".", "-")
+   
 input_file = csv.DictReader(open("People.csv"))
 
 input_file_licitadores = csv.DictReader(open("../RegistroLicitadoresYEmpresasClasificadas/Empresas-2023-03-05.csv"))
@@ -19,13 +22,8 @@ output_graph = ConjunctiveGraph(identifier="https://data.ehu.eus/bcitr/people/gr
 # TODO: https://github.com/mikel-egana-aranguren/BasqueCountryInstitutionsTransparentRelationsGraph/issues/1
 base_uri = 'https://data.ehu.eus/bcitr/'
 person_uri = base_uri + 'person/'
-schema_person_uri = 'https://schema.org/Person'
-role_uri = 'https://data.ehu.eus/bcitr/role/' # https://data.ehu.eus/bcitr/role/[normalised-person-name][officer|president|director|...][dates]
+role_uri = 'https://data.ehu.eus/bcitr/role/'
 company_uri = base_uri + "company/"
-
-has_role_prop = 'http://ehu.eus/tro#has_role'
-person_name_prop = "http://www.w3.org/ns/person#name"
-person_profile_prop = "http://ehu.eus/tro#profile_url"
 
 for row in input_file:
     # for k, v in row.items():
@@ -33,24 +31,44 @@ for row in input_file:
     name = row['name']
     profile = row['profile']
     entity = row['entity']
+    role = row['role']
+    startdate = row['start-date']
+    enddate = row['end-date']
+    entity = row['entity']
+    evidence = row['evidence']
 
     # Codigo repetido de Registro Licitadores: mala idea
     for row in input_file_licitadores:
         entity_id = row['Número inscripción']
         entity_name = row['Denominación social']
         if entity_name == entity:
-            print(entity_id)
             entity_uri = company_uri + entity_id
 
-
     # TODO: https://github.com/mikel-egana-aranguren/BasqueCountryInstitutionsTransparentRelationsGraph/issues/9
-    normalised_name = name.replace(" ", "-").lower()
+
+    normalised_name = normalise_name(name)
     person_uri_name = person_uri + normalised_name
     output_graph.add((URIRef(person_uri_name), RDFS.label, Literal(name)))
-    output_graph.add((URIRef(person_uri_name), RDF.type, URIRef(schema_person_uri)))
-    output_graph.add((URIRef(person_uri_name), URIRef(person_name_prop), Literal(name)))
-    output_graph.add((URIRef(person_uri_name), URIRef(person_profile_prop), Literal(profile)))
-    output_graph.add((URIRef(person_uri_name), URIRef(), URIRef()))
+    output_graph.add((URIRef(person_uri_name), RDF.type, URIRef('https://schema.org/Person')))
+    output_graph.add((URIRef(person_uri_name), URIRef("http://www.w3.org/ns/person#name"), Literal(name)))
+    output_graph.add((URIRef(person_uri_name), URIRef("http://ehu.eus/tro#profile_url"), Literal(profile)))
+
+    # https://data.ehu.eus/bcitr/role/[normalised-person-name][officer|president|director|...][dates]
+    role_def_uri = role_uri + normalised_name + "-" + normalise_name(role) + "-" + startdate + "-" + enddate + "-" + normalise_name(entity)[:-1] + ""
+
+    output_graph.add((URIRef(person_uri_name), URIRef("http://ehu.eus/tro#has_role"), URIRef(role_def_uri)))
+    if role == "Manager":
+        output_graph.add((URIRef(role_def_uri), RDF.type, URIRef("http://ehu.eus/tro#Manager")))
+    output_graph.add((URIRef(role_def_uri), URIRef("http://ehu.eus/tro#start_date"), Literal(startdate)))
+    output_graph.add((URIRef(role_def_uri), URIRef("http://ehu.eus/tro#end_date"), Literal(enddate)))
+    output_graph.add((URIRef(role_def_uri), URIRef("http://ehu.eus/tro#in_entity"), URIRef(entity_uri)))
+    output_graph.add((URIRef(role_def_uri), URIRef("http://ehu.eus/tro#with_evidence"), URIRef(evidence)))
+
+    # TODO: generalizar
+    output_graph.add((URIRef(evidence), RDF.type, URIRef("http://ehu.eus/tro#PublicProfile")))
+    output_graph.add((URIRef(evidence), URIRef("http://ehu.eus/tro#url"), Literal(evidence)))
+
+
 
 output_graph.serialize(destination='People.nq', format='nquads', encoding="utf-8")
 
