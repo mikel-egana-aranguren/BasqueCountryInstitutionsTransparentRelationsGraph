919072010fd5b9a9b7440bdf9b8d5ea1763333c8
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2024-03-17 22:11:11 +0100

    trifid works

--- deployment/oxigraph/oxigraph_post_data.sh
+++ deployment/oxigraph/oxigraph_post_data.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
 curl http://localhost:7878/store?default -H 'Content-Type: text/turtle' -T ./$1
+
+# https://github.com/zazuko/trifid/issues/193#issuecomment-1992018917
+
+# curl -X POST http://localhost:7878/update \
+# -H "Content-Type: application/sparql-update" \
+# --data-binary 'PREFIX ex: <http://example.org/>
+#                INSERT DATA {
+#                  ex:subject ex:predicate ex:object .
+#                }'
